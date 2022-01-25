cb3a91c667adf5e9993c7f33fd7efe777870f837
imakiri <vanzha.alexander@gmail.com>
2022-01-25 21:00:38 +0300

    C27

--- projects/in_memory_notepad/stages/1uni/supervisor.go
+++ projects/in_memory_notepad/stages/1uni/supervisor.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/imakiri/golang-stepik/testlib"
 
+// Экспериментальная реализация крос-языкового тестировщика,
+// архитектура которого была описана Антоном
 func main() {
 	new(testlib.Main).Supervise()
 }
